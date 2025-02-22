6d9cd5a4bb62252084f416017915c0a5f73598e9
Yair Sepunaru <yairsep@gmail.com>
2025-02-22 20:59:52 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    // Set the response headers
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    // Write the response content
+    res.write('<h1>Hello, Node.js HTTP Server!</h1>');
+    res.end();
+});
+
+// Specify the port to listen on
+const port = 3001;
+
+// Start the server
+server.listen(port, () => {
+    console.log(`Node.js HTTP server is running on port ${port}`);
+});
